0a6e796d7f583335e3ddfc417f7a6233d0df8277
Zzu-h <juho7785@naver.com>
2023-01-08 00:35:24 +0900

    feat: navigation 조정
    - detail fragment에서 뒤로가기 눌렀을 때, 돌아가지 않음
    - 위의 문제 해결

--- ui/src/main/java/com/zzu/afreecatv/ui/detail/DetailFragment.kt
+++ ui/src/main/java/com/zzu/afreecatv/ui/detail/DetailFragment.kt
@@ -1,10 +1,14 @@
 package com.zzu.afreecatv.ui.detail
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.zzu.afreecatv.R
 import com.zzu.afreecatv.databinding.FragmentDetailBinding
 import com.zzu.afreecatv.domain.model.Broad
 
@@ -12,6 +16,8 @@ class DetailFragment : Fragment() {
 
     private var binding: FragmentDetailBinding? = null
     private val broad: Broad by lazy { arguments?.getSerializable("broad") as Broad }
+
+    private lateinit var callback: OnBackPressedCallback
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +34,19 @@ class DetailFragment : Fragment() {
         binding?.item = broad
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigate(R.id.action_DetailFragment_to_HomeFragment)
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDetach() {
+        super.onDetach()
+        callback.remove()
     }
 
     override fun onDestroyView() {
